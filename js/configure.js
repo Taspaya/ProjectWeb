37af23b4858446c52db236a538229b507b99d39d
Gerard <gerardarditcastells@enti.cat>
2020-05-14 17:29:29 +0200

    a cascarla

--- js/configure.js
+++ js/configure.js
@@ -10,12 +10,27 @@ function preloadImages()
     game.load.spritesheet('platform', 'assets/imgs/obstacleSprite.png', 100, 25, 5);
     game.load.spritesheet('playerDeath', 'assets/imgs/albondigaDeath.png', 60, 60, 5);
     game.load.image('slab', 'assets/imgs/obstacle.png');
+
+
     game.load.image('trap', 'assets/imgs/trap.png');
 
     game.load.image('virus', 'assets/imgs/virus.png');
 
 
     game.load.image('pup','assets/imgs/pup.png');
+
+
+
+
+
+    game.load.image('heart', 'assets/imgs/heart.png');
+    game.load.image('healthholder', 'assets/imgs/health_holder.png');
+    game.load.image('healthbar', 'assets/imgs/health_bar.png');
+    game.load.image('powerUp', 'assets/imgs/powerup.png');
+
+
+
+
 }
 
 
--- js/play.js
+++ js/play.js
@@ -36,11 +36,15 @@ const DOWNARROW_KEYCODE = 40;
 
 //#region GLOBALS
 
+const max_vida = 100;
+
 let juega = true;
 
 let virus;
 
 let pUp;
+
+let trap;
 
 let ball;
 let rightBTN;
@@ -56,18 +60,14 @@ let obstaclesGroup;
 let virusGroup;
 let pUpsGroup;
 
-//let obstacles;
-
-//let obstacle = new Image();
-//obstacle.src     = "assets/imgs/obstacle.png";
+let havePower = false;
 
 let gap;
 let gaps = [-2 , 1 , 3 , 0 , 0 , 0 , 1 , 2 , 1 ];
 
-let pups = [false , false , true , false , true , false , false , false , true ];
+let totalPlataformas = 10;
 
-let navesita = new Image();
-navesita.src     = "assets/imgs/nave.png";
+let pups = [false , false , true , false , true , false , false , false , true ];
 
 let rightArrowPressed = false,
     leftArrowPressed = false,
@@ -79,332 +79,7 @@ let continueGame = true;
 let Currentlifes = 3;   
 let counter = 0;
 
-
-//#endregion
-
-//window.onload = startGame;
-
-//#region HANDLERS
-
-function handlerOne(event)
-{
-    switch (event.keyCode) {
-        case RIGHTARROW_KEYCODE:
-            if (!rightArrowPressed) {
-                rightArrowPressed = true;
-                gameArea.obstacles.forEach(element => { element.setSpeedX(OBSTACLE_SPEEDX);});
-            }
-            break;
-        case LEFTARROW_KEYCODE:
-            if (!leftArrowPressed) {
-                leftArrowPressed = true;
-                gameArea.obstacles.forEach(element => { element.setSpeedX(-OBSTACLE_SPEEDX);});
-
-            }
-            break;
-        case UPARROW_KEYCODE:
-            if (!upArrowPressed) {
-                upArrowPressed = true;
-                theSquare.setSpeedY(-SQUARE_SPEED_Y);
-            }
-            break;
-        case DOWNARROW_KEYCODE:
-            if (!downArrowPressed) {
-                downArrowPressed = true;
-                theSquare.setSpeedY(SQUARE_SPEED_Y);
-            }
-            break;
-        default:
-            break;
-    }
-}
-
-function handlerTwo(event) {
-    
-    switch (event.keyCode) {
-        case RIGHTARROW_KEYCODE:
-            rightArrowPressed = false;
-            gameArea.obstacles.forEach(element => { element.setSpeedX(0);});
-            break;
-        case LEFTARROW_KEYCODE:
-            leftArrowPressed = false;
-            gameArea.obstacles.forEach(element => { element.setSpeedX(0);});
-            break;
-        case UPARROW_KEYCODE:
-            upArrowPressed = false;
-            theSquare.setSpeedY(0);
-            break;
-        case DOWNARROW_KEYCODE:
-            downArrowPressed = false;
-            theSquare.setSpeedY(0);
-            break;
-        default:
-            break;
-    }
-}
-
-
-//#endregion
-
-
-//#region old version
-function startGame() {
-
-    gameArea.initialise();
-    gameArea.setupLevel(0);
-    gameArea.render();
-
-    window.document.addEventListener("keydown", handlerOne);
-    window.document.addEventListener("keyup", handlerTwo);
-
-    seconds = 0;
-    theChrono = document.getElementById("chrono");
-    lifes = document.getElementById("lives");
-    Currentlifes = 3;
-}
-
-function updateGame() {
-
-
-
-    var velo = 5;
-    let collision = false;
-
-    global_accelerate();
-
-    for (let i = 0; i < gameArea.obstacles.length; i++) 
-    {
-        if (theSquare.crashWith(gameArea.obstacles[i]))
-        {
-            collision = true;
-            obs = i;
-            break;
-        }
-    }
-
-    if (collision)
-    {
-        if(gameArea.obstacles[obs].imagen != null)
-        {
-
-            rebound();
-            if(gameArea.obstacles[obs].trap == true)
-            punish();
-        }
-        else
-        {
-            endGame();
-        }
-    }
-    else {
-
-        gameArea.frameNumber += 1;
-        for (let i = gameArea.obstacles.length - 1; i >= 0; i--) 
-        {
-            gameArea.obstacles[i].move();
-            gameArea.obstacles[i].accelerate();
-             if (gameArea.obstacles[i].y + OBSTACLE_WIDTH <= 0) 
-             {
-                 gameArea.removeObstacle(i);
-            }
-        }
-        theSquare.setIntoArea(gameArea.canvas.width, gameArea.canvas.height);
-        gameArea.clear();
-        gameArea.render();
-    }
-}
-
-function endGame() {
-    continueGame = false;
-    clearInterval(gameArea.interval);
-    window.document.removeEventListener("keydown", handlerOne);
-    window.document.removeEventListener("keyup", handlerTwo);
-}
-
-function punish(_i)
-{
-    Currentlifes =  Currentlifes - 1;
-    lifes.innerHTML = Currentlifes;
-}
-
-function restartGame()
-{
-    for (let i = gameArea.obstacles.length - 1; i >= 0; i--)
-    {
-        gameArea.removeObstacle(i);
-    }
-
-    theSquare.y = 100;
-    theSquare.x = gameArea.canvas.width / 2;
-}
-
-function global_accelerate()
-{
-    let chance = Math.random();
-    if (chance < PROBABILITY_ACCELERATE) global_speed += GLOBAL_ACCELERATION;
-}
-
-class SquaredForm {
-
-    constructor(x, y, width, height, color, speedY ,imagen = null, trap = false) 
-    {
-
-        this.x = x; 
-        this.y = y;
-        this.width = width; 
-        this.height = height; 
-        this.color = color;
-        this.speedX = 0;
-        this.speedY = speedY;
-        this.imagen = imagen;
-        this.trap = trap;
-    }
-
-    setSpeedX(speedX) {
-        this.speedX = speedX;
-    }
-    setSpeedY(speedY) {
-        this.speedY = speedY;
-    }
-    render(ctx) 
-    {
-
-    if(this.imagen == null)
-    {
-        ctx.fillStyle = this.color;
-        ctx.fillRect(this.x, this.y, this.width, this.height);
-    }
-    else
-    {
-        ctx.drawImage (this.imagen, this.x,this.y, this.width, this.height);
-    }
-    }
-
-    accelerate()
-    {
-        this.speedY = global_speed; 
-    }
-    move() {
-
-        this.x += this.speedX;
-        this.y += this.speedY;
-    }
-
-    setIntoArea(endX, endY) {
-        this.x = Math.min(Math.max(0, this.x), (endX - this.width));
-        this.y = Math.min(Math.max(0, this.y), (endY - this.height));
-    }
-
-    crashWith(obj) {
-        // detect collision with the bounding box algorithm
-        let myleft = this.x;
-        let myright = this.x + this.width;
-        let mytop = this.y;
-        let mybottom = this.y + this.height;
-        let otherleft = obj.x;
-        let otherright = obj.x + obj.width;
-        let othertop = obj.y;
-        let otherbottom = obj.y + obj.height;
-        let crash = true;
-        if ((mybottom < othertop) || (mytop > otherbottom) || (myright < otherleft) ||
-            (myleft > otherright)) {
-            crash = false;
-        }
-        return crash;
-    }
-}
-
-class GameArea {
-
-    constructor(canvas, hero, obstacles) {
-        this.canvas = canvas;
-        this.hero = hero;
-        this.obstacles = obstacles;
-        this.context = null;
-        this.interval = null;
-        this.frameNumber = undefined;
-    }
-
-    initialise() 
-    {
-        this.canvas.width = GAME_AREA_WIDTH;
-        this.canvas.height = GAME_AREA_HEIGHT;
-        this.context = this.canvas.getContext("2d");
-        let theDiv = document.getElementById("platformGameStage");
-        theDiv.appendChild(this.canvas);
-        this.interval = setInterval(updateGame, 1000 / FPS);
-        this.frameNumber = 0;
-    }
-
-    render() {
-        for (const obstacle of this.obstacles) {
-            obstacle.render(this.context);
-        }
-        this.hero.render(this.context);
-    }
-    clear() {
-        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    }
-
-    addObstacle(obstacle) {
-        this.obstacles.push(obstacle);
-    }
-
-    removeObstacle(i) {
-        this.obstacles.splice(i, 1);
-    }
-
-    setupLevel(o)
-    {
-        let index = 0;
-        switch(o)
-        {
-            case 0:
-            for(var j = 301; j <= 3200; j++)
-            {
-
-                if(j == 3200)
-                {
-                    for(var i = -4; i <= 7; i++) 
-                    {
-                        if(i != gaps[index])
-                        {
-                            var color = "#4cd3c2";
-                            let form = new SquaredForm( (OBSTACLE_WIDTH * i), j, OBSTACLE_WIDTH , 25, color, global_speed);
-                            gameArea.addObstacle(form);
-                            form.setSpeedY(-OBSTACLE_SPEED)
-                        }
-                    }
-                }
-                else if(j % 300 == 0)
-                {
-                    console.log("STRUCTURE: " + index);
-                    // THIS FOR GENERATES THE STRUCTURE
-                    for(var i = -4; i <= 7; i++) 
-                    {
-                        if(i != gaps[index])
-                        {
-                            var color = "#4cd3c2";
-                            let form = new SquaredForm( (OBSTACLE_WIDTH * i), j, OBSTACLE_WIDTH , 25, color, global_speed ,obstacle);
-                            gameArea.addObstacle(form);
-                            form.setSpeedY(-OBSTACLE_SPEED)
-                        }
-                    }
-                    index++;
-                }
-            }
-                break;
-            default:
-                break;
-        }
-    }
-}
-
-
-let theSquare = new SquaredForm(GAME_AREA_WIDTH / 2, 100 , SQUARE_SIZE, SQUARE_SIZE, "#FF00CC", 0 ,navesita);
-let gameArea = new GameArea(document.createElement("canvas"), theSquare, []);
-let levelsData = ['assets/levels/level01.json', 'assets/levels/level02.json'];
-//#endregion
+//if(body.touching.down){}
 
 let playState = {
     create: createPlay,
@@ -417,7 +92,7 @@ function createPlay()
     rightBTN = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
 
     game.input.enabled = true;
-
+    havePower = false;
     obstaclesGroup = game.add.group();
     virusGroup = game.add.group();
     pUpsGroup = game.add.group();
@@ -426,6 +101,7 @@ function createPlay()
     pUpsGroup.enableBody = true;
     createKeyControls();
     createBall();
+    createHUD();
     createLevelOne();
 
 }
@@ -435,7 +111,8 @@ function updatePlay()
     game.physics.arcade.overlap(obstaclesGroup,ball, rebound, null, this);
     game.physics.arcade.overlap(virusGroup, ball, collapseVirus, null, this);
     game.physics.arcade.overlap(pUpsGroup, ball, getPowerUp, null, this);
-
+    
+    powerup();
 
     manageGravity();
 
@@ -497,7 +174,6 @@ function updatePlay()
     }
 }
 
-
 function manageGravity()
 {
 
@@ -550,14 +226,15 @@ function createBall()
     ball = game.add.sprite(x,y, 'ball');
 
 
+    ball.width = 30;
+    ball.height = 30;
     game.physics.arcade.enable(ball);
 
     ball.animations.add('pelota', [0,1,2,3,4,5,6,7], 14, true);
 
 }
 
-
-function createobstaclesGroup(distance,gap, powerup)
+function createobstaclesGroup(distance,gap, powerup, trap)
 {
     for(let i = -500,  j = -10; j < 18; i = i + 80, j++)
     {
@@ -571,9 +248,9 @@ function createobstaclesGroup(distance,gap, powerup)
 
 function getPowerUp(ball, _pup)
 {
-
     if(_pup.key = "pup")
     {
+        havePower = true;
         _pup.destroy();
         
         for(let i = 0; i < obstaclesGroup.children.length; i++)
@@ -593,16 +270,24 @@ function getPowerUp(ball, _pup)
             game.physics.arcade.enable(pUpsGroup.children[i]);
             pUpsGroup.children[i].body.velocity.y = pUpsGroup.children[i].body.velocity.y - 4;
         }
-    
-
     }
+}
+
+function powerup(){
+    var icon = hudGroup.children[3];
+ 
+    if (havePower){
+        icon.visible = true;
+    }
+    else icon.visible = false;
+  
 }
 
 function collapseVirus(ball, _virus)
 {
     if(_virus.key == "virus")
     {
-
+        updateHealthBar(.5);
         // AQUI FUNCIÓN DE LO DE BAJAR LA VIDA
 
         // AQUI ANMACIÓN DE DAÑO O LO QUE SEA
@@ -618,7 +303,7 @@ function rebound(ball, _obstacle)
 
     if(_obstacle.key == "slab")
     {
-
+        havePower = false;
         if(_obstacle.body.velocity.y < THRESHOLD) _obstacle.destroy();
 
         for(let i = 0; i < obstaclesGroup.children.length; i++)
@@ -652,11 +337,35 @@ function rebound(ball, _obstacle)
     
 }
 
+function updateHealthBar(damage) {
+
+    healthBar.scale.x -= .40 * damage; //Velocitat * 40% 
+    if (healthBar.scale.x <= 0) healthBar.scale.x = 0;
+
+}
+
+function createHUD() {
+
+    hudGroup = game.add.group();
+    hudGroup.create(5, 5, 'heart');
+    hudGroup.create(50, 15, 'healthholder');
+    healthBar = hudGroup.create(50,15,'healthbar');
+    hudNamePlayer=game.add.text(5,45,nombreJugador,{fill: '#FFFFFF', fontSize: 20});
+    hudPlatforms=game.add.text(345,760,totalPlataformas,{fill: '#FFFFFF', fontSize: 20});
+    hudGroup.create(320, 700, 'powerUp');
+    hudGroup.add(hudNamePlayer);
+
+    hudGroup.fixedToCamera = true;
+    healthValue = max_vida;
+
+}
+
 function createObstacle(x,y)
 {
     obstacle = game.add.sprite(x,y, 'slab');
     game.physics.arcade.enable(obstacle);
     obstacle.width = 80;
+    obstacle.height = 20;
     obstacle.body.velocity.y = -OBSTACLE_SPEED;
     obstaclesGroup.add(obstacle);
     
@@ -678,18 +387,30 @@ function createVirus(x,y)
 
 function createPowerUp(x, y)
 {
-
     pUp = game.add.sprite(x + 15,  y - 15,'pup');
     game.physics.arcade.enable(pUp);
     pUp.width = 40;
-    pUp.height = 50;
+    pUp.height = 60;
     pUp.body.velocity.y = -OBSTACLE_SPEED;
     pUp.body.velocity.x = 0;
     pUpsGroup.add(pUp);
 
 }
 
+function createTrap(x,y)
+{
+    trap = game.add.sprite(x + 15,  y - 15, 'trap');
+    game.physics.arcade.enable(trap);
+    trap.width = 40;
+    trap.height = 50;
+    trap.body.velocity.y = -OBSTACLE_SPEED;
+    trap.body.velocity.x = 0;
+    trap.add(trap);
+}
+
 function goToWelcome() {
     game.world.setBounds(0, 0, game.width, game.height);
     game.state.start('welcome');
 }
+
+
